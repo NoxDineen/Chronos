2e0a12f0ba826ebfe52cb541f9bd8999e541de24
Nox Dineen <noxdineen@gmail.com>
2012-03-01 14:01:30 -0500

    Added a .gitignore file to keep .pyc files from crufting up the repo

---
+++ chronos/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from timekeeper.chronos import *
+
+class PersonAdmin(admin.ModelAdmin):
+	list_display = ('first_name', 'last_name', 'email')
+	list_display_links = ('first_name', 'last_name', 'email')
+	list_filter = ('is_support',)
+	search_fields = ('first_name',)
+
+class AssignmentAdmin(admin.ModelAdmin):
+	list_filter = ('date',)
+	filter_horizontal = ('people',)
+
+class RoleAdmin(admin.ModelAdmin):
+	list_display = ('name', 'icon')
+
+
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Assignment, AssignmentAdmin)
+admin.site.register(Role, RoleAdmin)
--- chronos/models.py
+++ chronos/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Person(models.Model):
+	first_name = models.CharField(max_length=255)
+	last_name = models.CharField(max_length=255)
+	email = models.EmailField(max_length=255)
+	is_support = models.BooleanField(default=False)
+	user = models.ForeignKey(User, unique=True)
+
+	class Meta:
+		verbose_name_plural = 'people'
+
+	def __unicode__(self):
+		return self.email
+
+class Assignment(models.Model):
+	date = models.DateField
+	person = models.ForeignKey('chronos.Person')	
+
+	class Meta:
+		unique_together = ('date', 'person')
+
+	def __unicode__(self):
+		return self.date
+
+class Role(models.Model):
+	name = models.CharField(max_length=255)
+	assignment = models.ManyToManyField('chronos.Assignment')
+	icon = models.ImageField(upload_to='chronos')
+
+	def __unicode__(self):
+		return self.name
