eece4ac1e98300304245728438362baff10d6fb1
Nox Dineen <noxdineen@gmail.com>
2012-05-18 16:45:30 -0400

    Removes databaseinfo from settings.py

--- settings.py
+++ settings.py
@@ -9,16 +9,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'timedb',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -155,3 +145,5 @@ EMAIL_SUBJECT_PREFIX = '[Chronos] '
 EMAIL_USE_TLS = True
 
 AUTH_PROFILE_MODULE = 'chronos.Person'
+
+from settings_local import *
