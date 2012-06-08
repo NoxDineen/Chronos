5d6cdf65203d29dadbb03fcc45a60431701ea118
Nox Dineen <noxdineen@gmail.com>
2012-06-08 11:15:11 -0400

    Updates development settings template

--- timekeeper/settings_local_tmpl.py
+++ timekeeper/settings_local_tmpl.py
@@ -1,5 +1,3 @@
-import os
-
 DEBUG = True
 
 DATABASES = {
@@ -14,12 +12,27 @@ DATABASES = {
 
     'schedgy': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'test',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'NAME': 'schedgy',                      # Or path to database file if using sqlite3.
+        'USER': 'nox',                      # Not used with sqlite3.
+        'PASSWORD': 'art3mis42',                  # Not used with sqlite3.
+        'HOST': 'nas01.2ndsiteinc.com',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
+STATIC_ROOT =  ''
 
+MEDIA_ROOT = '/Users/nox/Code/Chimekeeper/media/'
+
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+     '/Users/nox/Code/ChronosApp/timekeeper/static/',
+)
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    '/users/nox/code/ChronosApp/timekeeper/templates/'
